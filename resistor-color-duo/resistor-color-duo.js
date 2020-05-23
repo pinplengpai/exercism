b63794e8b901e88516155e4a9d286d0f484560a0
Pin Plengpai <pinplengpai@gmail.com>
2020-05-23 12:46:18 +0700

    finish exercise resistor-color-duo

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+
+export const decodedValue = (names) => {
+  let concatDigits = (accu, value) => accu.concat(COLORS.indexOf(value));
+
+  return parseInt(names.slice(0, 2).reduce(concatDigits, ''));
 };
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
